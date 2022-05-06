83e9386be7e32d51dbc0e4403b0a48c4680ab3a2
THEGOLDENPRO <jassim7256@gmail.com>
2022-05-06 02:59:27 +0100

    changes

--- src/setup.py
+++ src/setup.py
@@ -4,14 +4,18 @@ setup(
   name='DevGoldy',
   version="1.0dev",
   description='My own custom server software I install on every server.', 
-  url='https://github.com', 
-  project_urls={"Bug Tracker": "https://github.com/Goldy-Bot/Goldy-Bot-V4/issues"}, 
+  url='https://github.com/THEGOLDENPRO/Dev-Goldy-Server-Software', 
+  project_urls={"Bug Tracker": "https://github.com/THEGOLDENPRO/Dev-Goldy-Server-Software/issues"}, 
   author='Dev Goldy', 
   author_email='goldy@novauniverse.net', 
   license='MIT',
-  keywords=["goldy bot", "Goldy Bot", "Goldy Bot V4", "Goldy Bot V3", "goldy"], 
   packages=find_packages(), 
   include_package_data=True,
   install_requires=open('requirements.txt').read(),
-  python_requires=">=3.8"
+  python_requires=">=3.8",
+
+  entry_points="""
+  [console_scripts]
+  devgoldy=DevGoldy:devgoldy
+  """
 )
